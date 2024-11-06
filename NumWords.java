dbf90a9a9f25b82ad36487811e5d980a74390efc
AmitErlich7 <amit.erlich@post.runi.ac.il>
2024-11-06 15:37:08 +0200

    finished NumWords

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,31 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int number = Integer.parseInt(args[0]);
+
+		while (number > 0) {
+			if (number > 100) {
+				String numberString = Integer.toString(number);
+				char firstDigit = numberString.charAt(0);
+				System.out.println(firstDigit + " hundreds");	
+				numberString = numberString.substring(1);
+				number = Integer.parseInt(numberString);
+			}
+			if (number > 10) {
+				String numberString = Integer.toString(number);
+				char firstDigit = numberString.charAt(0);
+				System.out.println(firstDigit + " tens");	
+				numberString = numberString.substring(1);
+				number = Integer.parseInt(numberString);
+			}
+			if (number > 1) {
+				String numberString = Integer.toString(number);
+				char firstDigit = numberString.charAt(0);
+				System.out.println(firstDigit + " ones.");	
+				numberString = numberString.substring(1);
+				number = Integer.parseInt(numberString);
+			}
+		}
+
 	}
 }
