bd7225526f1f7645c536fdf8c4c8abcb073be320
AmitErlich7 <amit.erlich@post.runi.ac.il>
2024-11-07 18:46:43 +0200

    output fix Ascend file

--- Ascend.java
+++ Ascend.java
@@ -3,9 +3,9 @@
 public class Ascend {
 	public static void main(String[] args) {
 		int max = Integer.parseInt(args[0]);
-		int a = (int)Math.random() * max;
-		int b = (int)Math.random() * max;
-		int c = (int)Math.random() * max;
+		int a = (int)(Math.random() * max);
+		int b = (int)(Math.random() * max);
+		int c = (int)(Math.random() * max);
 		int maxNum = Math.max(a, Math.max(b, c));
 		int minNum = Math.min(a, Math.min(b, c));
 		int midNum = a + b + c - maxNum - minNum; 
